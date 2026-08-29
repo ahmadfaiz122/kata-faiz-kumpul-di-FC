791adfebf81eee14de9019dad4ebd8ae2281fea9
razenephyrz <reinzalkinas@gmail.com>
2026-08-30 00:01:39 +0700

    fe

---
+++ frontend/src/lib/Card.svelte
@@ -0,0 +1,17 @@
+<script>
+</script>
+<div class="flex w-[50vh] h-[25vh] bg-off-white rounded-lg border border-l border-electric-cyan">
+    <div class=" h-full w-[50%] flex-col items-center">
+        <p class=" text-laser-pink font-black text-start ml">2 Hours</p>
+        <div class=" flex justify-evenly">
+            <p class="font-medium mr-3 text-gray-500">with </p>
+            <p class=" font-bold text-electric-cyan">Reinzal</p>
+        </div>    
+    </div>
+    <div class=" h-full w-[50%] bg-amber-500 ">
+        <div class=" h-[50] w-full space-x-1.5">
+            <p class=" text-laser-pink ">You can learn:</p>
+            <p>Sveltekit</p>
+        </div>
+    </div>
+</div>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(),
+    tailwindcss()
+  ],
 })
