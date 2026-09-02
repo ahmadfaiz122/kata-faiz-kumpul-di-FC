34e5372296651b810da5f0a0ea18387063688bf5
ahmadfaiz122 <ahmadfaizabdilla@gmail.com>
2026-09-02 13:17:19 +0700

    udah yaa

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -1,12 +1,14 @@
 <?php
 
 namespace App\Models;
+use Laravel\Sanctum\HasApiTokens;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use HasApiTokens;
 
 class User extends Authenticatable
 {
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -7,3 +7,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/auth/google/redirect', [GoogleAuthController::class, 'redirect'])->name('auth.google.redirect');
 Route::get('/auth/google/callback', [GoogleAuthController::class, 'callback'])->name('auth.google.callback');
+
+
