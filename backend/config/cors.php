a5b534559f3ceb606003b4791c293ddfa17592aa
razenephyrz <reinzalkinas@gmail.com>
2026-09-23 18:26:48 +0700

    last changes

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -6,9 +6,9 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        env('FRONTEND_URL', 'http://localhost:5173'),
+        env('FRONTEND_URL', 'https://sienna-donkey-308864.hostingersite.com'),
         'http://localhost:5173',
-        'http://127.0.0.1:5173',
+        'http://127.0.0.1:5173','https://',
     ],
 
     'allowed_origins_patterns' => [],
